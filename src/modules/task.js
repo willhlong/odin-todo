3ea7b3e571978faf50c3dfca141004f2a03ff62e
William Long <willhlong@gmail.com>
2024-02-04 23:08:35 -0500

    changed checkbox for task completed

--- src/modules/task.js
+++ src/modules/task.js
@@ -5,10 +5,8 @@ export default function createTask(description, dueDate) {
     el.classList.add('task-elements-container');
 
     const taskCompleted = document.createElement('div');
-    const checkBox = document.createElement('input');
-    checkBox.type = 'checkbox';
-    taskCompleted.classList.add('completed');
-    taskCompleted.appendChild(checkBox);
+    taskCompleted.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><title>circle</title><path d="M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2Z" /></svg>';
+    taskCompleted.classList.add('completed')
     const taskDescription = document.createElement('div');
     taskDescription.innerText = description;
     taskDescription.classList.add('description');
